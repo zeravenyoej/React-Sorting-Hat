28e63e0bcc8b9a16b96f7324cf50e540292735f8
Joey Nevarez <josephnevarez12@gmail.com>
2019-12-04 13:17:49 -0800

    greeting and background img are rendering

---
+++ react-sorting-hat/src/components/Greeting.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Greeting = ()=>{
+    return(
+        <div>
+            <h1>Greetings, student!</h1>
+            <h2>It's time to figure out to which house you belong.</h2>
+            <button>Start</button>
+        </div>
+    );
+};
+
+export default Greeting;
