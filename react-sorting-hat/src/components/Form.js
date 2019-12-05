84290b5341afedbc735b3749acfd2cac9375296a
Joey Nevarez <josephnevarez12@gmail.com>
2019-12-04 17:25:44 -0800

    added values, state, and functions to form

--- react-sorting-hat/src/components/Form.js
+++ react-sorting-hat/src/components/Form.js
@@ -1,6 +1,33 @@
-import React from 'react'
+import React, {useState} from 'react'
 
 const Form = (props)=> {
+
+    const [formValue, setFormValue] = useState({
+        brave: '',
+        studious: '',
+        witty: '',
+        resourceful: '',
+        ambitious: '',
+        hardWorking: '',
+    });
+
+    const handleChange = (e) => {
+        e.preventDefault();
+        setFormValue({...formValue, [e.target.name]:e.target.value});
+    }
+
+    const handleSubmit = e => {
+        e.preventDefault();
+        setFormValue({
+            brave: '',
+            studious: '',
+            witty: '',
+            resourceful: '',
+            ambitious: '',
+            hardWorking: '',
+        })
+    }
+
     return(
         <div>
             <form>
@@ -8,48 +35,60 @@ const Form = (props)=> {
                     1. Are you brave?
                     <input
                         type='text'
-                        name='text'
+                        name='brave'
                         placeholder='type yes or no'
+                        value={formValue.brave}
+                        onChange={event=>handleChange(event)}
                     /><pre/>
                 </label>
                 <label>
                     2. Are you studious?
                     <input
                         type='text'
-                        name='text'
+                        name='studious'
                         placeholder='type yes or no'
+                        value={formValue.studious}
+                        onChange={event=>handleChange(event)}
                     /> <pre/>
                 </label>
                 <label>
                     3. Are you witty?
                     <input
                         type='text'
-                        name='text'
+                        name='witty'
                         placeholder='type yes or no'
+                        value={formValue.witty}
+                        onChange={event=>handleChange(event)}
                     /> <pre/>
                 </label>
                 <label>
                     4. Are you resourceful?
                     <input
                         type='text'
-                        name='text'
+                        name='resourceful'
                         placeholder='type yes or no'
+                        value={formValue.resourceful}
+                        onChange={event=>handleChange(event)}
                     /> <pre/>
                 </label>
                 <label>
                     5. Are you ambitious?
                         <input
                         type='text'
-                        name='text'
+                        name='ambitious'
                         placeholder='type yes or no'
+                        value={formValue.ambitious}
+                        onChange={event=>handleChange(event)}
                         /><pre/>
                 </label>
                 <label>
-                    6. Are you witty?
+                    6. Are you hard working?
                     <input
                         type='text'
-                        name='text'
+                        name='hardWorking'
                         placeholder='type yes or no'
+                        value={formValue.hardWorking}
+                        onChange={event=>handleChange(event)}
                     /> <pre/>
                 </label>
                 <pre/>
